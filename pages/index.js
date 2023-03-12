d8d0ad598c50c367de0dff9af15a6d8c81631740
Anastasiia Obushenko <n.obushenko@gmail.com>
2023-03-12 16:28:24 -0400

    add layout to all pages

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
-import Layout from '@/components/Layout';
 import Products from '@/components/Products';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -10,11 +9,9 @@ const inter = Inter({ subsets: ['latin'] });
 export default function Home() {
   return (
     <>
-      <Layout>
-        <main className={styles.main}>
-          <Products />
-        </main>
-      </Layout>
+      <main className={styles.main}>
+        <Products />
+      </main>
     </>
   );
 }
