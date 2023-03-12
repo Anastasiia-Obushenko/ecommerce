5238b3aafcd4a57adcd8d81de48103e4d87ce4d9
Anastasiia Obushenko <n.obushenko@gmail.com>
2023-03-11 19:39:29 -0500

    list products

---
+++ components/Products.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { data } from '@/utils/data';
+import Image from 'next/image';
+
+const Products = () => {
+    return (
+        <div>{data.products.map((product) => (
+            <Image
+                key={product.id}
+                src={`/images${product.image}`}
+                height={230}
+                width={230}
+                style={{ objectFit: 'cover', height: '230px' }}
+                alt={product.title}
+            />
+        ))}</div>
+    );
+};
+
+export default Products;
--- utils/data.js
+++ utils/data.js
@@ -1,53 +1,53 @@
 export const data = {
     products: [
         {
-        id: 1,
-        title: 'Product 1',
-        description: 'Product 1 description',
-        price: 10000,
-        image:'../public/images/12681029_4Z_2101.w019.n001.436A.p30.436.jpg'
-    },
+            id: 1,
+            title: 'Product 1',
+            description: 'Product 1 description',
+            price: 10000,
+            image: '/12681029_4Z_2101.w019.n001.436A.p30.436.jpg'
+        },
         {
-        id: 2,
-        title: 'Product 2',
-        description: 'Product 2 description',
-        price: 10000,
-        image:'../public/images/12681029_4Z_2101.w019.n001.436A.p30.436.jpg'
-    },
+            id: 2,
+            title: 'Product 2',
+            description: 'Product 2 description',
+            price: 50000,
+            image: '/13513679_47.jpg'
+        },
         {
-        id: 3,
-        title: 'Product 3',
-        description: 'Product 3 description',
-        price: 10000,
-        image:'../public/images/12681029_4Z_2101.w019.n001.436A.p30.436.jpg'
-    },
+            id: 3,
+            title: 'Product 3',
+            description: 'Product 3 description',
+            price: 30000,
+            image: '/7552426_31.jpg'
+        },
         {
-        id: 4,
-        title: 'Product 4',
-        description: 'Product 4 description',
-        price: 10000,
-        image:'../public/images/12681029_4Z_2101.w019.n001.436A.p30.436.jpg'
-    },
+            id: 4,
+            title: 'Product 4',
+            description: 'Product 4 description',
+            price: 20000,
+            image: '/abstract-g17c84fad3_1920.jpg'
+        },
         {
-        id: 5,
-        title: 'Product 5',
-        description: 'Product 5 description',
-        price: 10000,
-        image:'../public/images/12681029_4Z_2101.w019.n001.436A.p30.436.jpg'
-    },
+            id: 5,
+            title: 'Product 5',
+            description: 'Product 5 description',
+            price: 10000,
+            image: '/pexels-mo-eid-11592813.jpg'
+        },
         {
-        id: 6,
-        title: 'Product 6',
-        description: 'Product 6 description',
-        price: 10000,
-        image:'../public/images/12681029_4Z_2101.w019.n001.436A.p30.436.jpg'
-    },
+            id: 6,
+            title: 'Product 6',
+            description: 'Product 6 description',
+            price: 20000,
+            image: '/pexels-rodion-kutsaiev-7911758.jpg'
+        },
         {
-        id: 7,
-        title: 'Product 7',
-        description: 'Product 7 description',
-        price: 10000,
-        image:'../public/images/12681029_4Z_2101.w019.n001.436A.p30.436.jpg'
-    }
+            id: 7,
+            title: 'Product 7',
+            description: 'Product 7 description',
+            price: 10000,
+            image: '/pexels-tony-litvyak-12915478.jpg'
+        }
     ]
-}
+};
