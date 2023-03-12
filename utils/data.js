e6910c6915dc5fc87c5d06638229f05c18ead912
Anastasiia Obushenko <n.obushenko@gmail.com>
2023-03-11 19:26:18 -0500

    typo fix

--- utils/data.js
+++ utils/data.js
@@ -6,42 +6,42 @@ export const data = {
         description: 'Product 1 description',
         price: 10000,
         image:'../public/images/12681029_4Z_2101.w019.n001.436A.p30.436.jpg'
-    }
+    },
         {
         id: 2,
         title: 'Product 2',
         description: 'Product 2 description',
         price: 10000,
         image:'../public/images/12681029_4Z_2101.w019.n001.436A.p30.436.jpg'
-    }
+    },
         {
         id: 3,
         title: 'Product 3',
         description: 'Product 3 description',
         price: 10000,
         image:'../public/images/12681029_4Z_2101.w019.n001.436A.p30.436.jpg'
-    }
+    },
         {
         id: 4,
         title: 'Product 4',
         description: 'Product 4 description',
         price: 10000,
         image:'../public/images/12681029_4Z_2101.w019.n001.436A.p30.436.jpg'
-    }
+    },
         {
         id: 5,
         title: 'Product 5',
         description: 'Product 5 description',
         price: 10000,
         image:'../public/images/12681029_4Z_2101.w019.n001.436A.p30.436.jpg'
-    }
+    },
         {
         id: 6,
         title: 'Product 6',
         description: 'Product 6 description',
         price: 10000,
         image:'../public/images/12681029_4Z_2101.w019.n001.436A.p30.436.jpg'
-    }
+    },
         {
         id: 7,
         title: 'Product 7',
