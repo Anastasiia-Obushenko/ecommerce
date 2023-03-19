0cd16b1371e024bf8a84865864fe4a4ba0041f96
Anastasiia Obushenko <n.obushenko@gmail.com>
2023-03-18 21:11:16 -0400

    typo fix

--- utils/db.js
+++ utils/db.js
@@ -38,7 +38,7 @@ function convertDocToObj(doc) {
     doc._id = doc._id.toString();
     doc.createdAt = doc.createdAt.toString();
     doc.updatedAt = doc.updatedAt.toString();
-    return dic;
+    return doc;
 }
 
 const db = { connect, disconnect, convertDocToObj };
